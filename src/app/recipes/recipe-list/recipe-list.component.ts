c92c0cfadd79d30eb6a2f2772a2ef41c909b5e5d
tercette <leandro.tercette@gmail.com>
2022-09-16 12:01:23 -0300

    styling recipe list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,9 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a image', 'https://skinnyms.com/wp-content/uploads/2018/04/Traditional-Ratatouille-1.jpg'),
     new Recipe('A Test Recipe', 'This is simply a image', 'https://skinnyms.com/wp-content/uploads/2018/04/Traditional-Ratatouille-1.jpg')
+
   ];
 
   constructor() { }
