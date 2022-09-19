c5ef77ba3adfb47e1554445a78d1768cb4d08e3a
tercette <leandro.tercette@gmail.com>
2022-09-19 09:54:00 -0300

    ingredients seted in recipe detail

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 import { Recipe } from './recipe.model';
 
 
@@ -6,9 +7,33 @@ export class RecipeService {
 recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a image', 'https://skinnyms.com/wp-content/uploads/2018/04/Traditional-Ratatouille-1.jpg'),
-    new Recipe('Another Test Recipe', 'This is simply a image', 'https://www.simplyrecipes.com/thmb/1lLEOxwEeSdA6pGOpkicwj8UHUM=/720x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2012__07__grilled-sweet-potatoes-horiz-b-1600-3f2cdd1b50654a5b83a2b67a9b94735a.jpg')
-  ];
+    new Recipe(
+      'Torta de presunto e queijo',
+      'tortas',
+      'https://p2.trrsf.com/image/fget/cf/548/0/images.terra.com/2020/11/11/escondidinho-de-frigideira-768x512.jpg',
+      [
+        new Ingredient('ovos',3),
+        new Ingredient('caixa de leite',1),
+        new Ingredient('batata cozida assada',1),
+        new Ingredient('colher de sopa de margarina',2),
+        new Ingredient('xicara de cha de farinha de trigo',1),
+        new Ingredient('colheres de queijo parmesao ralado',2),
+        new Ingredient('sal, pimenta-do reino e oregano a gosto',3),
+      ]),
+    new Recipe(
+      'Pao de queijo',
+      'petiscos',
+      'https://uploads.metropoles.com/wp-content/uploads/2020/11/05213836/pao-de-queijo-batata-doce-600x400.jpg',
+      [
+        new Ingredient('ovos',2),
+        new Ingredient('xícaras (chá) de queijo de minas padrão (meia cura) ralado',2),
+        new Ingredient('xícara (chá) de óleo',2),
+        new Ingredient('xícara (chá) de leite',1),
+        new Ingredient('250g de polvilho doce (aproximadamente)',1),
+        new Ingredient('250g de polvilho azedo',1),
+        new Ingredient('Óleo para untar',1),
+      ])
+    ];
 
   getRecipes(){
     return this.recipes.slice();
