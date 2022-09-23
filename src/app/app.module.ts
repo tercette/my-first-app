12daed86bfe9e1fd798d4f9b1f85a5514dcf2eb0
tercette <leandro.tercette@gmail.com>
2022-09-23 15:03:28 -0300

    issue reloading fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { RecipeService } from './recipes/recipe.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -35,7 +36,7 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
         ReactiveFormsModule,
         AppRoutingModule
     ],
-    providers: [ShoppingListService],
+    providers: [ShoppingListService, RecipeService],
     bootstrap: [AppComponent]
 })
 export class AppModule { }
