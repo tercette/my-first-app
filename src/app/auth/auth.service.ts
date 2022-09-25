05b5d45ed7bb571df7149f133288bfe537f50bec
tercette <leandro.tercette@gmail.com>
2022-09-25 20:01:05 -0300

    signup request implemented

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,8 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from "@angular/core";
-import { root } from "rxjs/internal-compatibility";
 
-@Injectable({providedIn: 'root'})
+interface AuthResponseData {
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  espiresIn: string;
+  localId: string;
+
+}
+
+@Injectable({ providedIn: 'root' })
 export class AuthService {
 
-  
+  constructor(private http: HttpClient) { }
+
+  signup(email: string, password: string) {
+    return this.http.post<AuthResponseData>(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDJnZQahGdLOZUKTZvgvTYEI3oe0Hj7-GI',
+      {
+        email: email,
+        password: password,
+        returnSecureToken: true
+      }
+    );
+  }
 }
