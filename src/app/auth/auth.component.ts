a8efd43a815669ffe55e94a3e260cc15c41dfe44
tercette <leandro.tercette@gmail.com>
2022-09-25 20:16:33 -0300

    error logic

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -8,6 +8,8 @@ import { NgForm } from "@angular/forms";
 })
 export class AuthComponent {
   isLoginMode = true;
+  isLoading = false;
+  error: string = null;
 
   constructor(private authService: AuthService) { }
 
@@ -21,17 +23,21 @@ export class AuthComponent {
     }
     const email = form.value.email;
     const password = form.value.password;
-
+    this.isLoading = true;
     if(this.isLoginMode) {
       // ...
     }else {
       this.authService.signup(email, password).subscribe(resData => {
         console.log(resData)
+        this.isLoading = false;
       },
         error => {
           console.log(error)
+          this.error = 'An error Occour'
+          this.isLoading = false;
         }
-      );
+        );
+        
     }
 
     form.reset();
