7a53768321ae82b998958e6d49b9e9d1f0dcc922
tercette <leandro.tercette@gmail.com>
2022-09-25 21:16:46 -0300

    sending login Request

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,6 +1,7 @@
-import { AuthService } from './auth.service';
+import { AuthResponseData, AuthService } from './auth.service';
 import { Component } from "@angular/core";
 import { NgForm } from "@angular/forms";
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-auth',
@@ -23,22 +24,27 @@ export class AuthComponent {
     }
     const email = form.value.email;
     const password = form.value.password;
+
+    let authObs: Observable<AuthResponseData>;
+
     this.isLoading = true;
     if (this.isLoginMode) {
-      // ...
+      authObs = this.authService.login(email, password);
     } else {
-      this.authService.signup(email, password).subscribe(resData => {
+      authObs =  this.authService.signup(email, password)
+    }
+
+    authObs.subscribe(
+      resData => {
         console.log(resData)
         this.isLoading = false;
       },
-        errorMessage => {
-          console.log(errorMessage);
-          this.error = errorMessage;
-          this.isLoading = false;
-        }
-      );
-
-    }
+      errorMessage => {
+        console.log(errorMessage);
+        this.error = errorMessage;
+        this.isLoading = false;
+      }
+    );
 
     form.reset();
   }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,13 +3,13 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from "@angular/core";
 import { throwError } from "rxjs";
 
-interface AuthResponseData {
+export interface AuthResponseData {
   idToken: string;
   email: string;
   refreshToken: string;
   espiresIn: string;
   localId: string;
-
+  registered?: boolean;
 }
 
 @Injectable({ providedIn: 'root' })
@@ -27,15 +27,26 @@ export class AuthService {
       }
     ).pipe(catchError(errorRes => {
       let errorMessage = "An unknow error occurred!"
-      if(!errorRes.error || !errorRes.error.error){
+      if (!errorRes.error || !errorRes.error.error) {
         return throwError(errorMessage);
       }
       switch (errorRes.error.error.message) {
-        case 'EMAIL_EXISTS' :
+        case 'EMAIL_EXISTS':
           errorMessage = 'This email exists already';
       }
       return throwError(errorMessage);
     })
     );
+  }
+
+  login(email: string, password: string) {
+    return this.http.post<AuthResponseData>(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDJnZQahGdLOZUKTZvgvTYEI3oe0Hj7-GI',
+      {
+        email: email,
+        password: password,
+        returnSecureToken: true
+      }
+    )
   }
 }
