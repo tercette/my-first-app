58772d601376b5fbc05d2e97e079efc1b0a7326f
tercette <leandro.tercette@gmail.com>
2022-09-27 14:57:42 -0300

    pre loading strategy

--- src/app/app-rounting.module.ts
+++ src/app/app-rounting.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
+import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
 import { AuthComponent } from './auth/auth.component';
 
 const appRoutes: Routes = [
@@ -16,7 +16,8 @@ const appRoutes: Routes = [
 ]
 
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes)],
+  imports: [RouterModule.forRoot(appRoutes, { preloadingStrategy: PreloadAllModules })
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
