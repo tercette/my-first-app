9c7a4e17fd187c1d32cda55628bbfc1f6e831060
tercette <leandro.tercette@gmail.com>
2022-09-28 16:08:06 -0300

    upgrading and deleting actions

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,10 +1,12 @@
+
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shoppingList.service';
-
+import { Store } from '@ngrx/store';
+import * as ShoppingListActions from '../shopping-list/store/shopping-list.actions'
 
 @Injectable()
 export class RecipeService {
@@ -26,7 +28,7 @@ export class RecipeService {
   // ];
   private recipes: Recipe[] = [];
 
-  constructor(private slService: ShoppingListService) {}
+  constructor(private slService: ShoppingListService, private store: Store<{ShoppingList: { ingredients: Ingredient[] }}>) {}
 
   setRecipes(recipes: Recipe[]) {
     this.recipes = recipes;
@@ -42,7 +44,8 @@ export class RecipeService {
   }
 
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
-    this.slService.addIngredients(ingredients);
+    //this.slService.addIngredients(ingredients);
+    this.store.dispatch(new ShoppingListActions.AddIngredients(ingredients))
   }
 
   addRecipe(recipe: Recipe) {
--- src/app/shopping-list/store/shopping-list.actions.ts
+++ src/app/shopping-list/store/shopping-list.actions.ts
@@ -1,12 +1,37 @@
 import { Ingredient } from './../../shared/ingredient.model';
-import { Action } from "@ngrx/store";
+import { Action } from '@ngrx/store';
 
 export const ADD_INGREDIENT = 'ADD_INGREDIENT';
+export const ADD_INGREDIENTS = 'ADD_INGREDIENTS';
+export const UPDATE_INGREDIENTS = 'UPDATE_INGREDIENTS';
+export const DELETE_INGREDIENTS = 'DELETE_INGREDIENTS';
 
 export class AddIngredient implements Action {
- readonly type = ADD_INGREDIENT;
+  readonly type = ADD_INGREDIENT;
 
- constructor(public payload : Ingredient) {
-  
- }
+  constructor(public payload: Ingredient) {}
 }
+
+export class AddIngredients implements Action {
+  readonly type = ADD_INGREDIENTS;
+
+  constructor(public payload: Ingredient[]) {}
+}
+
+export class UpdateIngredient implements Action {
+  readonly type = UPDATE_INGREDIENTS;
+
+  constructor(public payload: { index: number; ingredient: Ingredient }) {}
+}
+
+export class DeleteIngredient implements Action {
+  readonly type = DELETE_INGREDIENTS;
+
+  constructor(public payload: number) {}
+}
+
+export type ShoppingListActions =
+  | AddIngredient
+  | AddIngredients
+  | UpdateIngredient
+  | DeleteIngredient;
